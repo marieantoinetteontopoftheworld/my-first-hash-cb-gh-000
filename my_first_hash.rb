f5886ec9ceb0540a43d3bbe4d57cc9356310e3fd
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-05 13:02:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,8 +12,8 @@ def shipping_manifest
   #fill that hash with key/value pairs that describe the following information:
   #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   shipping_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
     "oil paintings" => 3
   }
 
@@ -23,6 +23,7 @@ def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
