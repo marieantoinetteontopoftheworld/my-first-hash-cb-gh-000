b62e5c626c39ba4a7caf0ad3c428f556020010e8
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-05 12:57:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,16 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  var hash = {
+  hash = {
     "wolf mugs" => 10
   }
-  
+
 end
 
 
 def shipping_manifest
   #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  #fill that hash with key/value pairs that describe the following information:
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 
 
 end
@@ -23,11 +23,8 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  
+
   #your code here
   #remember to return the shipping_manifest hash
 
 end
-
-
-
